96805be285b4517aa86500b720d46f5a17efd444
crazy61 <julio16.sy@gmail.com>
2017-07-19 11:15:23 -0500

    Nuevo comentario

--- src/paquete/AlgoritmoNuevo.java
+++ src/paquete/AlgoritmoNuevo.java
@@ -4,7 +4,26 @@ public class AlgoritmoNuevo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			System.out.println("Hola Mundo o/");
+			
+			int i = 0;
+			int nota = 0;
+			boolean copia = true;
+			
+			do{
+			System.out.println("Tu nota es " + nota);	
+				if(i>5)
+				{
+					break;
+				}
+				i++;
+			}while(copia);
 	}
+	
+	public static void imprimirPrueba(){
+		System.out.println("Bla bla bla bla");
+		System.out.println("Lata++");
+	}
+	
 
 }
